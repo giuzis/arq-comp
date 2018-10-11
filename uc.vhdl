41fd5e5e5c15fd723881ec28dfe5da7f15fb09fb
mathFonseca <matheus.2016@alunos.utfpr.edu.br>
2018-10-10 22:02:59 -0300

    quase funcional, tem um erro no data out

--- uc.vhdl
+++ uc.vhdl
@@ -4,6 +4,7 @@ use ieee.numeric_std.all;
 
 entity uc is
 	port (
+			clk: in std_logic;
 			wr_en: in std_logic;
 			data_in: in unsigned(14 downto 0);
 			data_out: out unsigned(14 downto 0)
@@ -11,6 +12,14 @@ entity uc is
 end entity;
 
 architecture a_uc of uc is
+	signal data_out_s: unsigned(14 downto 0);
 begin
-	data_out <= data_in + "000000000000001" WHEN wr_en='1';-- ELSE "000000000000000";
+	process(wr_en,clk)
+		begin
+			if wr_en='1' then
+			elsif rising_edge(clk) then
+			data_out_s <= data_in + "000000000000001";
+		end if;
+		end process;
+		data_out <= data_out_s;
 end architecture;
--- uc_tb.vhdl
+++ uc_tb.vhdl
@@ -8,30 +8,35 @@ end entity;
 architecture a_uc_tb of uc_tb is
 	component uc is
 		port (
+			clk: in std_logic;
 			wr_en: in std_logic;
 			data_in: in unsigned(14 downto 0);
 			data_out: out unsigned(14 downto 0)
 		);
 	end component;
-	signal wr_en_s: std_logic;
+	signal wr_en_s, clk_s: std_logic;
 	signal data_in_s, data_out_s: unsigned(14 downto 0);
 	begin
 		uut: uc port map (
 			wr_en=>wr_en_s,
+			clk=>clk_s,
 			data_in=>data_in_s,
 			data_out=>data_out_s
 		);
+	process --CLOCK
+		 begin
+		 clk_s <= '0';
+		 wait for 50 ns;
+		 clk_s <= '1';
+		 wait for 50 ns;
+	 end process;
 	process
 	begin
+		wr_en_s <= '0';
 		data_in_s <= "000000000000000";
 		wait for 50 ns;
 		wr_en_s <= '1';
-		wait for 50 ns;
-		wr_en_s <= '0';
-		wait for 50 ns;
-		wr_en_s <= '1';
-		wait for 50 ns;
-		data_in_s <= "000000000100000";
+		wait for 150 ns;
 		wait;
 	end process;
 end architecture;
